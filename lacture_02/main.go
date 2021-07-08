3f3430218ab0b2537da6110c20d452f36ad631a1
Faru02 <86843938+Faru02@users.noreply.github.com>
2021-07-08 07:21:30 -0700

    golang class_2

---
+++ lacture_02/main.go
@@ -0,0 +1,17 @@
+package main 
+
+
+import "fmt" 
+
+func main(){ 
+
+
+      fmt. Println ("Assalamualaikum")
+
+      fmt. Println ("I am Farhan Tanvir")
+      fmt. Println ("I am from Chittagong")
+
+      fmt.Println ("I am thankful to Rezaul bhai and Master Academy")
+      
+        
+     }
