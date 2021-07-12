28f0322b7bd6f80a2c50e836273fe126ee0222e4
Faru02 <86843938+Faru02@users.noreply.github.com>
2021-07-12 13:05:57 -0700

    3rd ..

---
+++ lecture_08/variable.go
@@ -0,0 +1,17 @@
+package main 
+
+import "fmt"
+
+func main(){
+
+  var name string = "FARHAN"
+  var name2 , city string = "TANVIR" , "CHITTAGONG"
+  fmt.Println(name , name2, city)
+
+  var c rune = 'A'
+  var age int = 19 
+  var result float32 = 58.365
+  
+   fmt.Println(c, age, result)
+
+  } 
